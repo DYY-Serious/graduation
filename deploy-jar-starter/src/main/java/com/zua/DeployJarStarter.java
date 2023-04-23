825959c764f438d8ade07cda2f5c83c28044039a
乔培洋 <1968833438@qq.com>
2023-04-23 14:21:15 +0800

    last Commit

--- deploy-jar-starter/src/main/java/com/zua/DeployJarStarter.java
+++ deploy-jar-starter/src/main/java/com/zua/DeployJarStarter.java
@@ -12,6 +12,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 public class DeployJarStarter {
     public static void main(String[] args) {
-        SpringApplication.run(DeployJarStarter.class);
+        SpringApplication.run(DeployJarStarter.class,args);
     }
 }
